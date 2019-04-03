b171581bda7a832bfbd0e376cfa3c166d2f4dbdc
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-03 18:19:55 +0100

    Create my intern page

---
+++ views/interns/ucheozoemena.php
@@ -0,0 +1,40 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="../app/img/interns/ucheozoemena.jpg" alt="Uche Ozoemena">
+                        <h6>Uche Ozoemena</h6>
+                        <p>Front-end Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/ucheozman?lang=en" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/CodeWithOz" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@ucheozoemena" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am a hard-working, highly motivated and self-taught programmer with 2 years of
+                           experience in building web interfaces that drive businesses results.
+                           I work with tools such as SASS, React, Redux, Node, Express, and Bootstrap, to
+                           name a few.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>CSS | SASS | JavaScript | React | Redux | Node | Express | PHP</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
