711e64250fc63c82aa2536d55cee7121210af7ef
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-03 18:27:01 +0100

    Update interns page to show my info

--- views/current.php
+++ views/current.php
@@ -13,15 +13,15 @@
      		<div class="row">
      			<div class="col-md-3 col-sm-6 col-xs-12">
      				<div class="intern-block border-blue">
-     					<img src="../app/img/about-talent.png" alt="">
-     					  <h6>Mark Essien</h6>
-                 <p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+     					<img src="../app/img/interns/ucheozoemena.jpg" alt="Uche Ozoemena">
+     					  <h6>Uche Ozoemena</h6>
+                 <p>Frontend developer, lover of JavaScript and React, all around nice guy.</p>
                  <ul class="list">
-                 <li><a href="#" target="_blank"<i class="fab fa-twitter"></i></a></li>
-                 <li><a href="#" target="_blank"<i class="fab fa-github"></i></a></li>
-                 <li><a href="#" target="_blank"<i class="fab fa-medium"></i></a></li>
+                 <li><a href="https://twitter.com/ucheozman?lang=en" target="_blank"<i class="fab fa-twitter"></i></a></li>
+                 <li><a href="https://github.com/CodeWithOz" target="_blank"<i class="fab fa-github"></i></a></li>
+                 <li><a href="https://medium.com/@ucheozoemena" target="_blank"<i class="fab fa-medium"></i></a></li>
                  </ul>
-     					    <a href="#" class="btn-view">view profile</a>
+     					    <a href="../interns/ucheozoemena" class="btn-view">view profile</a>
      				</div>
      			</div>
      			<div class="col-md-3 col-sm-6 col-xs-12">
